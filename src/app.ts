0c6677dfe93e61a9ada71d7c1a0b47132cbd89d5
Bishal Dofo <dofobishal@gmail.com>
2024-05-22 13:44:13 +0600

    All dependencies and require middlewire is installed

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { ProductRoutes } from './app/modules/product/product.route';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+// app routes
+app.use('/api/v1/products', ProductRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
+export default app;
---
+++ src/app/modules/product/product.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { ProductServices } from './product.service';
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const { product: productData } = req.body;
+    const result = await ProductServices.createProductIntoDB(productData);
+
+    res.status(200).json({
+      success: true,
+      message: 'Product created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const ProductControllers = {
+  createProduct,
+};
